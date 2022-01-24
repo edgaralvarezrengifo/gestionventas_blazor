1906d8edcc3140dab5600ea47994b1b02f829fa0
Edgar Alvarez <edgare192@hotmail.com>
2022-01-24 02:17:04 -0500

    se agrega lógica de registro de ventas

---
+++ PruebaGestionVentas/Shared/DTOs/NewSaleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGestionVentas.Shared.DTOs
+{
+    public class newSaleDTO
+    {
+        public int cantidad { get; set; }
+        public string ProductId;
+        public string ClientId;
+       
+    }
+}
